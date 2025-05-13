e0bb7d3365538bd11a25552ebaba4be9ef446c6b
Park YongJune <redplus@redplus.net>
2025-05-13 15:05:56 +0900

    Replace RepositoryMode enumeration

--- src/Azunt.ReasonManagement/Azunt.ReasonManagement/04_Extensions/ReasonServicesRegistrationExtensions.cs
+++ src/Azunt.ReasonManagement/Azunt.ReasonManagement/04_Extensions/ReasonServicesRegistrationExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Azunt.Models.Enums;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -9,14 +10,6 @@ namespace Azunt.ReasonManagement;
 /// </summary>
 public static class ReasonServicesRegistrationExtensions
 {
-    // 선택 가능한 저장소 모드 정의
-    public enum RepositoryMode
-    {
-        EfCore,
-        Dapper,
-        AdoNet
-    }
-
     /// <summary>
     /// ReasonApp 모듈의 서비스를 등록합니다.
     /// </summary>
