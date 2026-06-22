9fe19a8a4a25732b7cfe60b6f4e569dc482076df
Park YongJune <redplus@redplus.net>
2026-06-22 10:24:44 +0900

    Convert to file-scoped namespace

--- src/Azunt.ReasonManagement/Azunt.ReasonManagement/01_Models/Reason.cs
+++ src/Azunt.ReasonManagement/Azunt.ReasonManagement/01_Models/Reason.cs
@@ -1,44 +1,43 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Azunt.ReasonManagement
+namespace Azunt.ReasonManagement;
+
+/// <summary>
+/// Reasons 테이블과 일대일로 매핑되는 모델 클래스입니다.
+/// </summary>
+[Table("Reasons")]
+public class Reason
 {
     /// <summary>
-    /// Reasons 테이블과 일대일로 매핑되는 모델 클래스입니다.
+    /// 이용 사유 고유 아이디, 자동 증가
     /// </summary>
-    [Table("Reasons")]
-    public class Reason
-    {
-        /// <summary>
-        /// 이용 사유 고유 아이디, 자동 증가
-        /// </summary>
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public long Id { get; set; }
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long Id { get; set; }
 
-        /// <summary>
-        /// 활성 상태 표시, 기본값 true (활성)
-        /// </summary>
-        public bool? Active { get; set; }
+    /// <summary>
+    /// 활성 상태 표시, 기본값 true (활성)
+    /// </summary>
+    public bool? Active { get; set; }
 
-        /// <summary>
-        /// 레코드 생성 시간
-        /// </summary>
-        public DateTimeOffset CreatedAt { get; set; }
+    /// <summary>
+    /// 레코드 생성 시간
+    /// </summary>
+    public DateTimeOffset CreatedAt { get; set; }
 
-        /// <summary>
-        /// 레코드 생성자 이름
-        /// </summary>
-        public string? CreatedBy { get; set; }
+    /// <summary>
+    /// 레코드 생성자 이름
+    /// </summary>
+    public string? CreatedBy { get; set; }
 
-        /// <summary>
-        /// 이용 사유 이름 또는 제목
-        /// </summary>
-        public string? Name { get; set; }
+    /// <summary>
+    /// 이용 사유 이름 또는 제목
+    /// </summary>
+    public string? Name { get; set; }
 
-        /// <summary>
-        /// 이용 사유 상세 내용
-        /// </summary>
-        public string? Content { get; set; }
-    }
+    /// <summary>
+    /// 이용 사유 상세 내용
+    /// </summary>
+    public string? Content { get; set; }
 }
